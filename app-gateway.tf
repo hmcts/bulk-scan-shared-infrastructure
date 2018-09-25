0ee3c130b945ae7083378cedfa3fbbbee17daf8b
Tim Jacomb <tim.jacomb@hmcts.net>
2018-09-25 10:28:01 +0100

    Add vars

--- app-gateway.tf
+++ app-gateway.tf
@@ -1,45 +1,45 @@
 data "azurerm_key_vault_secret" "cert" {
-  name = "${var.external_cert_name}"
+  name      = "${var.external_cert_name}"
   vault_uri = "${var.external_cert_vault_uri}"
 }
 
 module "appGw" {
-  source = "git@github.com:hmcts/cnp-module-waf?ref=stripDownWf"
-  env = "${var.env}"
-  subscription = "${var.subscription}"
-  location = "${var.location}"
-  wafName = "${var.product}"
+  source            = "git@github.com:hmcts/cnp-module-waf?ref=stripDownWf"
+  env               = "${var.env}"
+  subscription      = "${var.subscription}"
+  location          = "${var.location}"
+  wafName           = "${var.product}"
   resourcegroupname = "${azurerm_resource_group.rg.name}"
-  team_name = "${var.team_name}"
-  team_contact = "${var.team_contact}"
-  destroy_me = "false"
+  team_name         = "${var.team_name}"
+  team_contact      = "${var.team_contact}"
+  destroy_me        = "false"
 
   # vNet connections
   gatewayIpConfigurations = [
     {
-      name = "internalNetwork"
+      name     = "internalNetwork"
       subnetId = "${data.azurerm_subnet.subnet_a.id}"
     },
   ]
 
   sslCertificates = [
     {
-      name = "${var.external_cert_name}"
-      data = "${data.azurerm_key_vault_secret.cert.value}"
+      name     = "${var.external_cert_name}"
+      data     = "${data.azurerm_key_vault_secret.cert.value}"
       password = ""
-    }
+    },
   ]
 
   # Http Listeners
   httpListeners = [
     {
-      name = "https-listener"
+      name                    = "https-listener"
       FrontendIPConfiguration = "appGatewayFrontendIP"
-      FrontendPort = "frontendPort443"
-      Protocol = "Https"
-      SslCertificate = "${var.external_cert_name}"
-      hostName = "${var.external_hostname}"
-    }
+      FrontendPort            = "frontendPort443"
+      Protocol                = "Https"
+      SslCertificate          = "${var.external_cert_name}"
+      hostName                = "${var.external_hostname}"
+    },
   ]
 
   # Backend address Pools
@@ -57,40 +57,40 @@ module "appGw" {
 
   backendHttpSettingsCollection = [
     {
-      name = "backend-443"
-      port = 443
-      Protocol = "Https"
-      CookieBasedAffinity = "Disabled"
-      AuthenticationCertificates = "ilbCert"
-      probeEnabled = "True"
-      probe = "https-probe"
+      name                           = "backend-443"
+      port                           = 443
+      Protocol                       = "Https"
+      CookieBasedAffinity            = "Disabled"
+      AuthenticationCertificates     = "ilbCert"
+      probeEnabled                   = "True"
+      probe                          = "https-probe"
       PickHostNameFromBackendAddress = "False"
-      Host = "${var.external_hostname}"
-
-    }
+      Host                           = "${var.external_hostname}"
+    },
   ]
+
   # Request routing rules
   requestRoutingRules = [
     {
-      name = "https"
-      RuleType = "Basic"
-      httpListener = "https-listener"
-      backendAddressPool = "${var.product}-${var.env}"
+      name                = "https"
+      RuleType            = "Basic"
+      httpListener        = "https-listener"
+      backendAddressPool  = "${var.product}-${var.env}"
       backendHttpSettings = "backend-443"
-    }
+    },
   ]
 
   probes = [
     {
-      name = "https-probe"
-      protocol = "Https"
-      path = "/"
-      interval = 30
-      timeout = 30
-      unhealthyThreshold = 5
+      name                                = "https-probe"
+      protocol                            = "Https"
+      path                                = "/"
+      interval                            = 30
+      timeout                             = 30
+      unhealthyThreshold                  = 5
       pickHostNameFromBackendHttpSettings = "false"
-      backendHttpSettings = "backend-443"
-      host = "${var.external_hostname}"
-    }
+      backendHttpSettings                 = "backend-443"
+      host                                = "${var.external_hostname}"
+    },
   ]
 }
--- palo-alto.tf
+++ palo-alto.tf
@@ -1,5 +1,12 @@
 module "palo_alto" {
-  source = "git@github.com:hmcts/terraform-module-palo-alto.git"
-  env = "${var.env}"
+  source  = "git@github.com:hmcts/terraform-module-palo-alto.git"
+  env     = "${var.env}"
   product = "${var.product}"
+
+  untrusted_vnet_name           = "core-infra-vnet-sandbox"
+  untrusted_vnet_resource_group = "core-infra-sandbox"
+  untrusted_vnet_subnet_name    = "core-infra-subnet-0-sandbox"
+  trusted_vnet_name             = "core-infra-vnet-sandbox"
+  trusted_vnet_resource_group   = "core-infra-sandbox"
+  trusted_vnet_subnet_name      = "palo-mgmt-sandbox"
 }
