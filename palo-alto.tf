6e01e5a2fb9b63ef97315a07ebb04ca7bcacf77b
Tim Jacomb <tim.jacomb@hmcts.net>
2018-09-26 14:54:21 +0100

    Use new untrusted subnet

--- palo-alto.tf
+++ palo-alto.tf
@@ -4,10 +4,10 @@ module "palo_alto" {
   product = "${var.product}"
   common_tags = "${var.common_tags}"
 
-  untrusted_vnet_name           = "core-infra-vnet-sandbox"
-  untrusted_vnet_resource_group = "core-infra-sandbox"
-  untrusted_vnet_subnet_name    = "core-infra-subnet-0-sandbox"
-  trusted_vnet_name             = "core-infra-vnet-sandbox"
-  trusted_vnet_resource_group   = "core-infra-sandbox"
-  trusted_vnet_subnet_name      = "palo-mgmt-sandbox"
+  untrusted_vnet_name           = "core-infra-vnet-${var.env}"
+  untrusted_vnet_resource_group = "core-infra-${var.env}"
+  untrusted_vnet_subnet_name    = "palo-untrusted-${var.env}"
+  trusted_vnet_name             = "core-infra-vnet-${var.env}"
+  trusted_vnet_resource_group   = "core-infra-${var.env}"
+  trusted_vnet_subnet_name      = "palo-mgmt-${var.env}"
 }
