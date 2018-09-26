85f954bb01a48bc379a160219a33dd8007bcd131
Tim Jacomb <tim.jacomb@hmcts.net>
2018-09-26 15:07:24 +0100

    Add destination ip

--- palo-alto.tf
+++ palo-alto.tf
@@ -1,7 +1,7 @@
 module "palo_alto" {
-  source  = "git@github.com:hmcts/terraform-module-palo-alto.git"
-  env     = "${var.env}"
-  product = "${var.product}"
+  source      = "git@github.com:hmcts/terraform-module-palo-alto.git"
+  env         = "${var.env}"
+  product     = "${var.product}"
   common_tags = "${var.common_tags}"
 
   untrusted_vnet_name           = "core-infra-vnet-${var.env}"
@@ -10,4 +10,5 @@ module "palo_alto" {
   trusted_vnet_name             = "core-infra-vnet-${var.env}"
   trusted_vnet_resource_group   = "core-infra-${var.env}"
   trusted_vnet_subnet_name      = "palo-mgmt-${var.env}"
+  trusted_destination_ip        = "10.100.140.11"              // sandbox ase ip currently, change to blob storage once thats done
 }
