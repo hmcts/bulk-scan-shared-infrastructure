02c06e5bcc3e70209001d1ef3a1e31a666373b0e
Tim Jacomb <tim.jacomb@hmcts.net>
2018-10-03 17:08:16 +0100

    Update storage type

--- storage-account.tf
+++ storage-account.tf
@@ -13,7 +13,7 @@ resource "azurerm_storage_account" "storage_account" {
   resource_group_name = "${azurerm_resource_group.rg.name}"
 
   location                 = "${azurerm_resource_group.rg.location}"
-  account_tier             = "Premium"
+  account_tier             = "Standard"
   account_replication_type = "LRS"
   account_kind             = "BlobStorage"
 
