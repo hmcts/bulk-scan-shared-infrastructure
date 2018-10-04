e2e60a612f851c7b6c3ea1de4f944c6e9335ac5e
Tim Jacomb <tim.jacomb@hmcts.net>
2018-10-04 21:32:34 +0100

    try not set type

--- storage-account.tf
+++ storage-account.tf
@@ -34,12 +34,10 @@ resource "azurerm_storage_container" "bulkscan_container" {
   name                  = "bulkscan"
   resource_group_name   = "${azurerm_storage_account.storage_account.resource_group_name}"
   storage_account_name  = "${azurerm_storage_account.storage_account.name}"
-  container_access_type = "blob"
 }
 
 resource "azurerm_storage_container" "sscs_container" {
   name                  = "sscs"
   resource_group_name   = "${azurerm_storage_account.storage_account.resource_group_name}"
   storage_account_name  = "${azurerm_storage_account.storage_account.name}"
-  container_access_type = "blob"
 }
