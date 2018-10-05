7350e4c83aaf3634830858f6f5b22f466e27c7f6
Tim Jacomb <tim.jacomb@hmcts.net>
2018-10-05 16:15:37 +0100

    Set trusted_destination_host

--- palo-alto.tf
+++ palo-alto.tf
@@ -17,5 +17,5 @@ module "palo_alto" {
   trusted_vnet_name             = "core-infra-vnet-${var.env}"
   trusted_vnet_resource_group   = "core-infra-${var.env}"
   trusted_vnet_subnet_name      = "palo-mgmt-${var.env}"
-  trusted_destination_ip        = "10.100.140.11"              // sandbox ase ip currently, change to blob storage once thats done
+  trusted_destination_host      = "${azurerm_storage_account.storage_account.name}.blob.core.windows.net"
 }
