284228ebe713364f68f89ec5a4cf65e862aff56a
Tim Jacomb <tim.jacomb@hmcts.net>
2018-10-10 09:32:25 +0100

    Nuke firewall stuff

--- storage-account.tf
+++ storage-account.tf
@@ -17,11 +17,6 @@ resource "azurerm_storage_account" "storage_account" {
   account_replication_type = "LRS"
   account_kind             = "BlobStorage"
 
-  network_rules {
-    virtual_network_subnet_ids = ["${data.azurerm_subnet.trusted_subnet.id}"]
-    bypass                     = ["Logging", "Metrics", "AzureServices"]
-  }
-
   custom_domain {
     name          = "${var.external_hostname}"
     use_subdomain = false
@@ -30,19 +25,14 @@ resource "azurerm_storage_account" "storage_account" {
   tags = "${local.tags}"
 }
 
-// Can't create containers currently as jenkins doesn't have the service endpoint enabled
-// Enabling it for jenkins would block it accessing other accounts
-// so need to work on getting that rolled out
-// For now containers will be created manually
+resource "azurerm_storage_container" "bulkscan_container" {
+  name                  = "bulkscan"
+  resource_group_name   = "${azurerm_storage_account.storage_account.resource_group_name}"
+  storage_account_name  = "${azurerm_storage_account.storage_account.name}"
+}
 
-//resource "azurerm_storage_container" "bulkscan_container" {
-//  name                  = "bulkscan"
-//  resource_group_name   = "${azurerm_storage_account.storage_account.resource_group_name}"
-//  storage_account_name  = "${azurerm_storage_account.storage_account.name}"
-//}
-//
-//resource "azurerm_storage_container" "sscs_container" {
-//  name                  = "sscs"
-//  resource_group_name   = "${azurerm_storage_account.storage_account.resource_group_name}"
-//  storage_account_name  = "${azurerm_storage_account.storage_account.name}"
-//}
+resource "azurerm_storage_container" "sscs_container" {
+  name                  = "sscs"
+  resource_group_name   = "${azurerm_storage_account.storage_account.resource_group_name}"
+  storage_account_name  = "${azurerm_storage_account.storage_account.name}"
+}
