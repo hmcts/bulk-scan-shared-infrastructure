65643e24e0034adc97dedba3b15d1bc3c46f667c
Luigi Bitonti <luigi.bitonti@hmcts.net>
2018-11-14 17:04:32 +0000

    Fix tags definition

--- application-insights.tf
+++ application-insights.tf
@@ -3,7 +3,7 @@ resource "azurerm_application_insights" "appinsights" {
   location            = "${var.appinsights_location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   application_type    = "${var.application_type}"
-  common_tags         = "${var.common_tags}"
+  tags                = "${var.common_tags}"
 }
 
 output "appInsightsInstrumentationKey" {
