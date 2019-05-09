dc262a8e256ef69ee29267e2fd3b0cc9ecb70a31
Donatas Martinkus <doncem@users.noreply.github.com>
2019-05-09 12:36:33 +0100

    Not use env var for composer files

--- bin/start-ccd-web.sh
+++ bin/start-ccd-web.sh
@@ -44,12 +44,11 @@ az acr login --name hmcts --subscription ${SUBSCRIPTION_ID}
 # Compose CCD Web
 #####################################################################################
 
-COMPOSE_FILES="-f docker-compose.yml"
-docker-compose ${COMPOSE_FILES} up ${@} -d ccd-case-management-web \
-                                           dm-store \
-                                           ccd-api-gateway \
-                                           idam-api \
-                                           authentication-web \
-                                           smtp-server \
-                                           ccd-importer \
-                                           idam-importer
+docker-compose -f docker-compose.yml up ${@} -d ccd-case-management-web \
+                                                dm-store \
+                                                ccd-api-gateway \
+                                                idam-api \
+                                                authentication-web \
+                                                smtp-server \
+                                                ccd-importer \
+                                                idam-importer
