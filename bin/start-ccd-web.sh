dff9933be5d27d2745dd541ba9faa1155ac3e5f4
Donatas Martinkus <doncem@users.noreply.github.com>
2019-05-13 20:56:04 +0100

    Add more info while containers are building

--- bin/start-ccd-web.sh
+++ bin/start-ccd-web.sh
@@ -45,6 +45,8 @@ az acr login --name hmcts --subscription ${SUBSCRIPTION_ID}
 # Compose CCD Web
 #####################################################################################
 
+echo "Starting docker containers..."
+
 docker-compose -f docker-compose.yml up ${@} -d ccd-case-management-web \
                                                 dm-store \
                                                 ccd-api-gateway \
@@ -53,6 +55,12 @@ docker-compose -f docker-compose.yml up ${@} -d ccd-case-management-web \
                                                 smtp-server \
                                                 ccd-importer \
                                                 idam-importer
+
+while [[ `docker ps -a | grep starting | wc -l | awk '{$1=$1};1'` != "0" ]]
+do
+    echo "Waiting for " `docker ps -a | grep starting | wc -l | awk '{$1=$1};1'` " containers to start. Sleeping for 5 seconds..."
+    sleep 5
+done
 
 # for convenience, let's have personal caseworker account
 
