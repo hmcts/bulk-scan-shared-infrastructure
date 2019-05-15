78b1da7cb74514917a1bfa7df4e944f4aa1c5899
Donatas Martinkus <doncem@users.noreply.github.com>
2019-05-15 11:21:45 +0100

    Switch to bulkscan caseworker by default

--- bin/create-case-worker.sh
+++ bin/create-case-worker.sh
@@ -7,7 +7,7 @@ FORENAME="${2:-Case}"
 SURNAME="${3:-Worker}"
 PASSWORD="Password12"
 USER_GROUP="caseworker"
-USER_ROLES='[{"code":"caseworker-sscs"}]' # Role has to be present in idam and service related
+USER_ROLES='[{"code":"caseworker-bulkscan"}]' # Role has to be present in idam and service related
 
 binFolder=$(dirname "$0")
 
