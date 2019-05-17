a4d936d3436e531415861de79fd90b5f93444bec
Donatas Martinkus <doncem@users.noreply.github.com>
2019-05-17 11:01:07 +0100

    Move to separate scripts

--- bin/start-ccd-web.sh
+++ bin/start-ccd-web.sh
@@ -57,18 +57,10 @@ done
 
 # for convenience, let's have personal caseworker account
 
-curl -XPOST \
-  http://localhost:8080/testing-support/accounts \
-  -H "Content-Type: application/json" \
-  -d '{"email":"'${ACCOUNT_EMAIL}'","forename":"Myself","surname":"As A Caseworker","password":"password","levelOfAccess":1, "roles": ["caseworker-bulkscan","caseworker"], "userGroup": {"code": "caseworker"}}'
+BIN_DIR=$(dirname "$0")
+
+${BIN_DIR}/create-case-worker.sh "$ACCOUNT_EMAIL" "Myself" "As A Caseworker"
 
 # in order to log in, we need user profiles present in the ccd
 
-SERVICE_TOKEN="$($(dirname "$0")/create-service-auth-token.sh ccd_data)"
-
-curl -XPUT \
-  http://localhost:4453/user-profile/users \
-  -H "Content-Type: application/json" \
-  -H "ServiceAuthorization: Bearer $SERVICE_TOKEN" \
-  -H "actionedBy: shell-script" \
-  -d '[{"id":"'${ACCOUNT_EMAIL}'","jurisdictions":[{"id":"BULKSCAN"}],"work_basket_default_jurisdiction":"BULKSCAN","work_basket_default_case_type":"BULKSCAN_ExceptionRecord","work_basket_default_state":"ScannedRecordReceived"}]'
+${BIN_DIR}/create-ccd-user-profile.sh "$ACCOUNT_EMAIL"
