a934c6a4f214fbf3ff1c3cf0a8940ce782593944
mdayican <mustafa.dayican@hmcts.net>
2019-09-17 13:46:47 +0100

    Revert "replace vault_uri with key_vault_id and format"
    This reverts commit 2c1e647a73ff0264fe22fd29f13e3157529d9a76.

--- queue.tf
+++ queue.tf
@@ -47,58 +47,58 @@ module "payments-queue" {
 
 # region connection strings and other shared queue information as Key Vault secrets
 resource "azurerm_key_vault_secret" "envelopes_queue_send_conn_str" {
-  name          = "envelopes-queue-send-connection-string"
-  value         = "${module.envelopes-queue.primary_send_connection_string}"
-  key_vault_id  = "${data.azurerm_key_vault.key_vault.vault_uri}"
+  name      = "envelopes-queue-send-connection-string"
+  value     = "${module.envelopes-queue.primary_send_connection_string}"
+  key_vault_id = "${data.azurerm_key_vault.key_vault.vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "envelopes_queue_listen_conn_str" {
-  name          = "envelopes-queue-listen-connection-string"
-  value         = "${module.envelopes-queue.primary_listen_connection_string}"
-  key_vault_id  = "${data.azurerm_key_vault.key_vault.vault_uri}"
+  name      = "envelopes-queue-listen-connection-string"
+  value     = "${module.envelopes-queue.primary_listen_connection_string}"
+  key_vault_id = "${data.azurerm_key_vault.key_vault.vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "envelopes_queue_max_delivery_count" {
-  name          = "envelopes-queue-max-delivery-count"
-  value         = "${var.envelope_queue_max_delivery_count}"
-  key_vault_id  = "${data.azurerm_key_vault.key_vault.vault_uri}"
+  name      = "envelopes-queue-max-delivery-count"
+  value     = "${var.envelope_queue_max_delivery_count}"
+  key_vault_id = "${data.azurerm_key_vault.key_vault.vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "notifications_queue_send_conn_str" {
-  name          = "notifications-queue-send-connection-string"
-  value         = "${module.notifications-queue.primary_send_connection_string}"
-  key_vault_id  = "${data.azurerm_key_vault.key_vault.vault_uri}"
+  name      = "notifications-queue-send-connection-string"
+  value     = "${module.notifications-queue.primary_send_connection_string}"
+  key_vault_id = "${data.azurerm_key_vault.key_vault.vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "notifications_queue_listen_conn_str" {
-  name          = "notifications-queue-listen-connection-string"
-  value         = "${module.notifications-queue.primary_listen_connection_string}"
-  key_vault_id  = "${data.azurerm_key_vault.key_vault.vault_uri}"
+  name      = "notifications-queue-listen-connection-string"
+  value     = "${module.notifications-queue.primary_listen_connection_string}"
+  key_vault_id = "${data.azurerm_key_vault.key_vault.vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "processed_envelopes_queue_send_conn_str" {
-  name          = "processed-envelopes-queue-send-connection-string"
-  value         = "${module.processed-envelopes-queue.primary_send_connection_string}"
-  key_vault_id  = "${data.azurerm_key_vault.key_vault.vault_uri}"
+  name      = "processed-envelopes-queue-send-connection-string"
+  value     = "${module.processed-envelopes-queue.primary_send_connection_string}"
+  key_vault_id = "${data.azurerm_key_vault.key_vault.vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "processed_envelopes_queue_listen_conn_str" {
-  name          = "processed-envelopes-queue-listen-connection-string"
-  value         = "${module.processed-envelopes-queue.primary_listen_connection_string}"
-  key_vault_id  = "${data.azurerm_key_vault.key_vault.vault_uri}"
+  name      = "processed-envelopes-queue-listen-connection-string"
+  value     = "${module.processed-envelopes-queue.primary_listen_connection_string}"
+  key_vault_id = "${data.azurerm_key_vault.key_vault.vault_uri}"
 }
 
 
 resource "azurerm_key_vault_secret" "payments_queue_send_conn_str" {
-  name          = "payments-queue-send-connection-string"
-  value         = "${module.payments-queue.primary_send_connection_string}"
-  key_vault_id  = "${data.azurerm_key_vault.key_vault.vault_uri}"
+  name      = "payments-queue-send-connection-string"
+  value     = "${module.payments-queue.primary_send_connection_string}"
+  key_vault_id = "${data.azurerm_key_vault.key_vault.vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "payments_queue_listen_conn_str" {
-  name          = "payments-queue-listen-connection-string"
-  value         = "${module.payments-queue.primary_listen_connection_string}"
-  key_vault_id  = "${data.azurerm_key_vault.key_vault.vault_uri}"
+  name      = "payments-queue-listen-connection-string"
+  value     = "${module.payments-queue.primary_listen_connection_string}"
+  key_vault_id = "${data.azurerm_key_vault.key_vault.vault_uri}"
 }
 
 # endregion
