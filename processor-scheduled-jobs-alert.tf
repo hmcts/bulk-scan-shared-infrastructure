f3a5578ea8c9d3ce7228971a4e2c26b782ac94a6
Aliveni Choppa <aliveni.ch@gmail.com>
2020-06-10 13:25:34 +0100

    Add alert for incomplete envelopes task

--- processor-scheduled-jobs-alert.tf
+++ processor-scheduled-jobs-alert.tf
@@ -102,3 +102,24 @@ module "bulk-scan-orchestrator-notifications-alert" {
   trigger_threshold          = 0
   resourcegroup_name         = "${azurerm_resource_group.rg.name}"
 }
+
+module "bulk-scan-incomplete-envelopes-alert" {
+  source            = "git@github.com:hmcts/cnp-module-metric-alert"
+  location          = "${azurerm_application_insights.appinsights.location}"
+  app_insights_name = "${azurerm_application_insights.appinsights.name}"
+
+  enabled    = "${var.env == "prod"}"
+  alert_name = "Bulk_Scan_Incomplete_Envelopes_-_BSP"
+  alert_desc = "Triggers when no logs from incomplete-envelopes-monitoring job found within timeframe."
+
+  app_insights_query = "traces | where message startswith 'Started incomplete-envelopes-monitoring job'"
+
+  frequency_in_minutes       = 120
+  time_window_in_minutes     = 120
+  severity_level             = "1"
+  action_group_name          = "${module.alert-action-group.action_group_name}"
+  custom_email_subject       = "Bulk Scan incomplete-envelopes-monitoring scheduled job alert"
+  trigger_threshold_operator = "Equal"
+  trigger_threshold          = 0
+  resourcegroup_name         = "${azurerm_resource_group.rg.name}"
+}
