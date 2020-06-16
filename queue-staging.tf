f5723cf08601b169599ff5931d7c4de67d61d6e4
apereverzin <andrey.pereverzin@hotmail.com>
2020-06-16 15:14:40 +0100

    Rolled back

--- queue-staging.tf
+++ queue-staging.tf
@@ -1,7 +1,3 @@
-locals {
-  staging-resource-count  = "1"
-}
-
 module "envelopes-staging-queue" {
   source              = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=master"
   name                = "envelopes-staging"
@@ -38,42 +34,42 @@ resource "azurerm_key_vault_secret" "envelopes_staging_queue_send_conn_str" {
   name      = "envelopes-staging-queue-send-connection-string"
   value     = "${module.envelopes-staging-queue.primary_send_connection_string}"
   vault_uri = "${data.azurerm_key_vault.key_vault.vault_uri}"
-  count     = "${local.staging-resource-count}"
+  count     = "${var.queue_secret_count}"
 }
 
 resource "azurerm_key_vault_secret" "envelopes_staging_queue_listen_conn_str" {
   name      = "envelopes-staging-queue-listen-connection-string"
   value     = "${module.envelopes-staging-queue.primary_listen_connection_string}"
   vault_uri = "${data.azurerm_key_vault.key_vault.vault_uri}"
-  count     = "${local.staging-resource-count}"
+  count     = "${var.queue_secret_count}"
 }
 
 resource "azurerm_key_vault_secret" "processed_envelopes_staging_queue_send_conn_str" {
   name      = "processed-envelopes-staging-queue-send-connection-string"
   value     = "${module.processed-envelopes-staging-queue.primary_send_connection_string}"
   vault_uri = "${data.azurerm_key_vault.key_vault.vault_uri}"
-  count     = "${local.staging-resource-count}"
+  count     = "${var.queue_secret_count}"
 }
 
 resource "azurerm_key_vault_secret" "processed_envelopes_staging_queue_listen_conn_str" {
   name      = "processed-envelopes-staging-queue-listen-connection-string"
   value     = "${module.processed-envelopes-staging-queue.primary_listen_connection_string}"
   vault_uri = "${data.azurerm_key_vault.key_vault.vault_uri}"
-  count     = "${local.staging-resource-count}"
+  count     = "${var.queue_secret_count}"
 }
 
 resource "azurerm_key_vault_secret" "payments_staging_queue_send_conn_str" {
   name      = "payments-staging-queue-send-connection-string"
   value     = "${module.payments-staging-queue.primary_send_connection_string}"
   vault_uri = "${data.azurerm_key_vault.key_vault.vault_uri}"
-  count     = "${local.staging-resource-count}"
+  count     = "${var.queue_secret_count}"
 }
 
 resource "azurerm_key_vault_secret" "payments_staging_queue_listen_conn_str" {
   name      = "payments-staging-queue-listen-connection-string"
   value     = "${module.payments-staging-queue.primary_listen_connection_string}"
   vault_uri = "${data.azurerm_key_vault.key_vault.vault_uri}"
-  count     = "${local.staging-resource-count}"
+  count     = "${var.queue_secret_count}"
 }
 
 # endregion
