d150598c878a5624bdaa5e7deb277a54f6fd0eeb
msl8r <72858053+msl8r@users.noreply.github.com>
2022-04-07 17:25:27 +0100

    DTSPO-6371 - use common_tags var

--- main.tf
+++ main.tf
@@ -2,26 +2,10 @@ provider "azurerm" {
   features {}
 }
 
-module "ctags" {
-  source      = "git@github.com:hmcts/terraform-module-common-tags.git?ref=master"
-  environment = var.environment
-  product     = var.product
-  builtFrom   = var.builtFrom
-}
-
-locals {
-  tags = merge(
-    module.ctags.common_tags,
-    tomap({
-      "Team Contact" = "#rbs"
-      "Team Name" = "Bulk Scan"
-    })
-  )
-}
 
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.env}"
   location = var.location
 
-  tags = local.tags
+  tags = var.common_tags
 }
