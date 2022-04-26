ae97e6908f374d80e44c450d775b0d4969fdd034
msl8r <72858053+msl8r@users.noreply.github.com>
2022-04-26 11:38:44 +0100

    DTSPO-6371 - upgrade azurerm to 3.2.0

--- queue.tf
+++ queue.tf
@@ -8,7 +8,7 @@ module "queue-namespace" {
 }
 
 module "envelopes-queue" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=master"
+  source              = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=DTSPO-6371_azurerm_upgrade"
   name                = "envelopes"
   namespace_name      = module.queue-namespace.name
   resource_group_name = azurerm_resource_group.rg.name
@@ -20,7 +20,7 @@ module "envelopes-queue" {
 }
 
 module "processed-envelopes-queue" {
-  source                                  = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=master"
+  source                                  = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=DTSPO-6371_azurerm_upgrade"
   name                                    = "processed-envelopes"
   namespace_name                          = module.queue-namespace.name
   resource_group_name                     = azurerm_resource_group.rg.name
@@ -30,7 +30,7 @@ module "processed-envelopes-queue" {
 }
 
 module "payments-queue" {
-  source                                  = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=master"
+  source                                  = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=DTSPO-6371_azurerm_upgrade"
   name                                    = "payments"
   namespace_name                          = module.queue-namespace.name
   resource_group_name                     = azurerm_resource_group.rg.name
