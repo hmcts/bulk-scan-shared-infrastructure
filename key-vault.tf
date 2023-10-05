f73fd06506254ddb5dca817ea7c5be1cff1df420
Rees Pozzi <rees.pozzi@justice.gov.uk>
2023-10-05 12:40:30 +0100

    Remove old MI references (#289)

--- key-vault.tf
+++ key-vault.tf
@@ -8,14 +8,8 @@ module "vault" {
   resource_group_name        = azurerm_resource_group.rg.name
   product_group_object_id    = "70de400b-4f47-4f25-a4f0-45e1ee4e4ae3"
   common_tags                = var.common_tags
-  managed_identity_object_id = var.managed_identity_object_id
   create_managed_identity    = true
 }
-
-# data "azurerm_key_vault" "key_vault" {
-#   name                = "${module.vault.key_vault_name}"
-#   resource_group_name = azurerm_resource_group.rg.name
-# }
 
 output "vaultName" {
   value = module.vault.key_vault_name
