402360c9818784bda3612cecbded62c6c99bd1ca
Ruth Bovell <ruthb111@outlook.com>
2025-01-08 15:35:35 +0000

    set azurerm and azured versions to same as reform scan shared infrastructure

--- state.tf
+++ state.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.0.0"
+      version = "3.106.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.40.0"
+      version = "2.48.0"
     }
   }
 }
