db0bed4c38991d1bd7bc40ad3bd07d69ababbaa8
Ruth Bovell <ruthb111@outlook.com>
2025-01-09 10:20:47 +0000

    FACT-1952 - adds service containers to account and staging account (#297)
    * adds service containers to account and staging account
    
    * Updating Terraform Formatting
    
    * updates azurerm
    
    * downgrades azurerm
    
    * upgrades azurerm
    
    * change deprecated variable to new one
    
    * Updating Terraform Formatting
    
    * play around with versioning
    
    * revert versions
    
    * set azurerm and azured versions to same as reform scan shared infrastructure
    
    * revert back to azurerm v3 variable
    
    * Updating Terraform Formatting
    
    ---------
    
    Co-authored-by: 58046 <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>

--- state.tf
+++ state.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.65.0"
+      version = "3.106.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.40.0"
+      version = "2.48.0"
     }
   }
 }
