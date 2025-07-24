a60d4f4141500fbedc7423f0d6165cb294b1a760
Jamie McConnell <jamie.mcconnell@justice.gov.uk>
2025-07-24 11:01:35 +0100

    Added import block for Managed Identity

---
+++ import.tf
@@ -0,0 +1,4 @@
+import {
+  to = module.vault.azurerm_user_assigned_identity.managed_identity[0]
+  id = "/subscriptions/7a4e3bd5-ae3a-4d0c-b441-2188fee3ff1c/resourceGroups/managed-identities-ithc-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/bulk-scan-ithc-mi"
+}
