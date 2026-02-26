3d9f31e946066369c1957af8024cc07429c6ab3b
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-02-26 13:14:15 +0000

    add ptl intsvc provider

--- provider.tf
+++ provider.tf
@@ -14,6 +14,13 @@ provider "azurerm" {
 
 provider "azurerm" {
   features {}
+  alias                      = "cft-ptl"
+  subscription_id            = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
+  skip_provider_registration = true
+}
+
+provider "azurerm" {
+  features {}
   alias           = "aks_preview"
   subscription_id = "8b6ea922-0862-443e-af15-6056e1c9b9a4"
 }
