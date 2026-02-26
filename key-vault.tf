d938a9d5db2cda8d8349292482d74fd8fac3eacf
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-02-26 12:54:34 +0000

    test branch

--- key-vault.tf
+++ key-vault.tf
@@ -1,5 +1,5 @@
 module "vault" {
-  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=DTSPO-29659/env-specific-access-policy"
   name                    = "${var.product}-${var.env}"
   product                 = var.product
   env                     = var.env
