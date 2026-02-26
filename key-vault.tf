6f50e0f12a9888bb3ad6c723f1daa5e0077b51ec
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-02-26 13:34:49 +0000

    remove jenkins variable

--- key-vault.tf
+++ key-vault.tf
@@ -5,7 +5,6 @@ module "vault" {
   env                     = var.env
   tenant_id               = var.tenant_id
   object_id               = var.jenkins_AAD_objectId
-  jenkins_object_id       = data.azurerm_user_assigned_identity.jenkins.principal_id
   resource_group_name     = azurerm_resource_group.rg.name
   product_group_object_id = "70de400b-4f47-4f25-a4f0-45e1ee4e4ae3"
   common_tags             = var.common_tags
